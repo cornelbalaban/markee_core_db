b0979faaa0a82d1690ffdc924f32b8ac56f3fcb6
Corneliu Balaban <corneliu.balaban@avira.com>
2020-12-22 15:42:50 +0200

    optimized imports

--- src/test/kotlin/DbTests.kt
+++ src/test/kotlin/DbTests.kt
@@ -1,12 +1,9 @@
 import models.User
-import models.User.emailUsr
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
 
 
 class DbTests {
